ffce54aa15f1f2b25afd6259e715d928af9fd13c
Soham Karanjikar <Soham@Sohams-MacBook-Pro.local>
2020-04-30 23:06:25 -0500

    remove comments and delete unused

--- cookiegenerator.py
+++ cookiegenerator.py
@@ -34,8 +34,8 @@ def initDriver():
         print("Failed to initialize driver with error: "+str(e))
 
 def randomString():
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(8+int(random.random()*7)))
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(8+int(random.random()*7.0)))
 
 
 def extractCookie(driver, name, value):
@@ -86,7 +86,7 @@ class botThread (threading.Thread):
         threading.Thread.__init__(self)
         self.threadID = threadID
     def run(self):
-        #f = open("cookies/cookie_batch_"+randomString()+".txt","w+")
+        f = open("cookies/cookie_batch_"+randomString()+".txt","w+")
         global stopthreads
         while True:
             try:
@@ -94,17 +94,16 @@ class botThread (threading.Thread):
                     print("Keyboard Interrupted on thread: "+str(self.threadID))
                     break
                 else:
-                    #driver = initDriver()
-                    #fillSignUpInfo(driver)
-                    #cookie = extractCookie(driver, '.ROBLOSECURITY', 'value')
-                    #f.write(cookie+"\n")
-                    #driver.quit()
-                    print("looping")
-                    time.sleep(.1)
+                    driver = initDriver()
+                    fillSignUpInfo(driver)
+                    cookie = extractCookie(driver, '.ROBLOSECURITY', 'value')
+                    f.write(cookie+"\n")
+                    driver.quit()
+                    time.sleep(15)
             except Exception as e:
                 print("Error while looping through run: "+str(e))
                 break
-                #f.close()
+                f.close()
             
         
 
